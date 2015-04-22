1ffab779c46ce428851d6e81e83c36acdd7b7239
Tomas Batalla <tebatalla@gmail.com>
2015-04-21 17:40:42 -0700

    Incorporating snake game into github page

--- js/snake-view.js
+++ js/snake-view.js
@@ -1,6 +1,42 @@
 (function() {
   window.Game = window.Game || {};
 
+  $.snakeGame = function (el) {
+    this.$game = $('<form class="game-play" action="index.html" method="post">' +
+          '<label>Number of rows? ' +
+            '<input type="text" class="rows" value="16">' +
+          '</label>' +
+          '<label>Difficulty ' +
+            '<select class="difficulty" name="">' +
+              '<option value="500">Easy</option>' +
+              '<option value="200" selected>Medium</option>' +
+              '<option value="100">Hard</option>' +
+              '<option value="50">Crazy Mode</option>' +
+            '</select>' +
+          '</label>' +
+          '<input type="submit" class="submit" value="Start the game!">' +
+        '</form>');
+    this.$el = $(el);
+    this.$el.html(this.$game);
+    this.$el.on('click', '.restart', this.restartGame.bind(this, event));
+    this.$el.on('submit', '.game-play', this.playGame.bind(this, event));
+  };
+
+  $.snakeGame.prototype = {
+    playGame: function(e, event){
+      event.preventDefault();
+      var interval = parseInt($('.difficulty').val());
+      var rows = parseInt($('.rows').val());
+      this.$el.html($('<div class="snake-board clearfix">'));
+      var view = new Game.View(this.$el.find('.snake-board'), rows, interval);
+    },
+
+    restartGame: function(e, event) {
+      event.preventDefault();
+      this.$el.html(this.$game);
+    },
+  };
+
   var View = window.Game.View = function($el, size, interval) {
     this.$el = $el;
     this.board = new Game.Board(size);
@@ -35,12 +71,11 @@
       this.$el.width(width);
     },
 
-    bindEvents: function() {
-      var that = this;
+    bindEvents: function () {
       $('body').keydown(this.handleKeyEvent.bind(this, event));
     },
 
-    step: function() {
+    step: function () {
       try {
         if (this.board.isNextMoveApple()) {
           this.board.snake.move(true);
@@ -53,10 +88,13 @@
       } catch(e) {
         clearInterval(this.interval);
         this.$el.after("<h1 class='game-over'>Game over!</h1>");
+        setTimeout(function() {
+          $('.game-over').after('<a href="#" class="restart">Play again?</a>');
+        }.bind(this), 1000);
       }
     },
 
-    draw: function() {
+    draw: function () {
       // this.$el.html('<pre>' + this.board.render() + '</pre>');
       var rows = "";
       var cells = "";
@@ -82,4 +120,10 @@
       this.$el.html(rows);
     }
   };
+
+  $.fn.snakeGame = function () {
+  return this.each(function () {
+    new $.snakeGame(this);
+  });
+};
 })();
